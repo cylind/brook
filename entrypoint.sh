6f6ba0abfa25e8e4cb0fde4adbd56cace6c8b873
Chan <chenyulin@yandex.com>
2022-06-02 00:02:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Download and install brook
+## Install brook
 wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
 
-# Run brook
-/usr/local/bin/brook wsserver --listen :${PORT} --password ${passwd}
+## Start service
+brook wsserver --listen :${PORT} --password ${passwd}
